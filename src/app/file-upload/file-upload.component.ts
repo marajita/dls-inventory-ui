0ad06612bd5fdaea65b416513bad3f8ccf5be706
smaharja <saru.maharjan@optum.com>
2020-06-17 23:43:05 -0400

    Upload feature added

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MessageService} from "primeng";
 import {UploadFileService} from "../upload-file.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import * as XLSX from "xlsx";
+
 
 @Component({
   selector: 'app-file-upload',
@@ -9,23 +12,62 @@ import {UploadFileService} from "../upload-file.service";
 })
 export class FileUploadComponent implements OnInit {
 
-  url:string = 'http://localhost:8080/api/v1/file-controller/upload'
 
-  constructor(private messageService: MessageService, private uploadFileService: UploadFileService) {}
+  uploadForm = new FormGroup({
+    file: new FormControl(null, Validators.required)
+  })
 
-  ngOnInit(): void {
-    console.log("init")
+
+  constructor(private messageService: MessageService, private uploadFileService: UploadFileService) {
   }
 
-  uploadedFiles: any[] = [];
+  ngOnInit(): void {
+  }
 
-  onUpload(event) {
-    console.log("upload file");
-    for(let file of event.files) {
-      this.uploadFileService.upload(file);
-      this.uploadedFiles.push(file);
+  uploadedFiles: File[] = [];
+  file: any;
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      this.uploadedFiles = event.target.files;
+      this.file = event.target.files[0];
+      console.log(this.file);
+      // this.form.get('file').setValue(file);
     }
+  }
 
-    this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
+  // onSubmit() {
+  //   console.log("submitting");
+  //   const fileReader = new FileReader();
+  //     fileReader.onload = (e) => {
+  //       this.arrayBuffer = fileReader.result;
+  //       const data = new Uint8Array(this.arrayBuffer);
+  //       const arr = new Array();
+  //       for(let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  //       const bstr = arr.join("");
+  //       const workbook = XLSX.read(bstr, {type:"binary"});
+  //       const first_sheet_name = workbook.SheetNames[0];
+  //       let worksheet = workbook.Sheets[first_sheet_name];
+  //       console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+  //     }
+  //     fileReader.readAsArrayBuffer(this.file);
+  // }
+
+  onSubmit() {
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(this.file);
+    reader.onload = (e: any) => {
+      /* create workbook */
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, {type: 'binary'});
+
+      /* selected the first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      const data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
+      console.log(data); // Data will be logged in array format containing objects
+    };
   }
 }
